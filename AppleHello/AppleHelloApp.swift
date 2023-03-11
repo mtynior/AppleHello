79b7d5a7cc043e592782bfea06033e8ee4e08cfd
Michal Tynior <michal.git@tynior.com>
2023-03-11 16:30:49 +0100

    Initial Commit

---
+++ AppleHello/AppleHelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleHelloApp.swift
+//  AppleHello
+//
+//  Created by Michal on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleHelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
